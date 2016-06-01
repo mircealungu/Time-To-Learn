fce69cf0435be30189948bb671fe9455cf269770
nbhaan <nbhaan93@gmail.com>
2016-06-01 13:25:10 +0200

    First prototype

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-var canvas, ctx;
+var canvasWords, canvasTime, ctxWords, ctxTime;
 var words = [], translations = [];
-var numberOfWords = 5;
+var numberOfWords = 20;
+
+//window.requestAnimationFrame = window.requestAnimationFrame ||
+//	window.webkitRequestAnimationFrame ||
+//	window.mozRequestAnimationFrame ||
+//	window.oRequestAnimationFrame ||
+//	window.msRequestAnimationFrame ||
+//	function(callback) {
+//    	'use strict';
+//    	window.setTimeout(callback, 1000 / 60);
+//	};
 
 function length(obj) {
     return Object.keys(obj).length;
@@ -47,41 +57,99 @@ function startNewSession() {
 	xhr.send(data);
 }
 
-function printOnScreen(string, posx, posy) {
+function printOnScreen(string, posx, posy, size) {
 	'use strict';
-	ctx.font = "20px Georgia";
-	ctx.fillStyle = "FFFFFF";
-	ctx.textAlign="center";
-	ctx.fillText(string, posx, posy);
+    ctxWords.font = size + "px Arial";
+	ctxWords.fillStyle = "#FFFFFF";
+	ctxWords.textAlign = "center";
+	ctxWords.fillText(string, posx, posy);
 }
+
+function getDate(){
+	'use strict';
+	var date;
+	try {
+		date = tizen.time.getCurrentDateTime();
+	} catch (err) {
+		console.error('Error: ', err.message);
+	}
+	return date;
+}
+
+function setBackground(hours){
+	if (hours < 8) {
+		document.getElementById("digitalTime").style.backgroundImage = "url('css/night.png')";
+		return;
+	} else if (hours < 13){
+		document.getElementById("digitalTime").style.backgroundImage = "url('css/morning.png')";
+		return;
+	} else if (hours < 20){
+		document.getElementById("digitalTime").style.backgroundImage = "url('css/afternoon.png')";
+		return;
+	} else if (hours < 24){
+		document.getElementById("digitalTime").style.backgroundImage = "url('css/evening.png')";
+		return;
+	}
+}
+
+function printDigitalTime() {
+	'use strict';
+	
+	var date = getDate();
+	
+	var hours = date.getHours();
+    var minutes = date.getMinutes();
+	
+	if(hours<10){
+		hours = "0" + hours;
+	}
+	if(minutes<10){
+		minutes = "0" + minutes;
+	}
+	
+	setBackground(hours);
+	
+	canvasTime = document.getElementById("digitalTime");
+    ctxTime = canvasTime.getContext("2d");    
+    ctxTime.clearRect(0,0,360,100);
+    
+    ctxTime.font = "40px Arial";
+	ctxTime.fillStyle = "black";
+	ctxTime.textAlign = "center";
+    ctxTime.fillText(hours + ":" + minutes, 180, 95);
+    
+    setTimeout("printDigitalTime()",1000);
+}
+
+//function revealClicked(){ 
+//    wordNumber++;
+//    canvas = document.getElementById("wordSpace");
+//    ctx = canvas.getContext("2d");
+//    ctx.clearRect(0,0,360,140);
+//    printOnScreen(translations[wordNumber], 180, 110, 20);
+//}
 
 window.onload = function() {
     'use strict';
-    
     var wordNumber = 0;
     
-	canvas = document.querySelector(".header");
-    ctx = canvas.getContext('2d');
-    ctx.font = "30px Arial";
-	ctx.fillStyle = "#FFFFFF";
-	ctx.textAlign = "center";
-    ctx.fillText("test", 180, 80);
+    printDigitalTime();
     
-	canvas = document.querySelector(".main");
-    ctx = canvas.getContext('2d');
+    canvasWords = document.getElementById("wordSpace");
+    ctxWords = canvasWords.getContext('2d');
     
     document.getElementById("nextButton").addEventListener("click", function(){
         wordNumber++;
         if (wordNumber > numberOfWords) {
         	wordNumber = 0;
         }
-    	ctx.clearRect(0,0, 380,380);
-    	printOnScreen(words[wordNumber], 180, 50);
+    	ctxWords.clearRect(0,0, 380,380);
+    	printOnScreen(words[wordNumber], 180, 70, 40);
         
     });
     
     document.getElementById("revealButton").addEventListener("click", function(){
-    	printOnScreen(translations[wordNumber], 180, 100);
+    	printOnScreen(translations[wordNumber], 180, 110, 20);
     });
 
     // add eventListener for tizenhwkey
@@ -94,8 +162,8 @@ window.onload = function() {
     });
     
     startNewSession();
-	printOnScreen(words[wordNumber], 180, 50);
-    //printOnScreen(translations[wordNumber], 180, 100);
+    printOnScreen(words[wordNumber], 180, 70, 40);
+    
     console.log(words);
     console.log(translations);
 };
